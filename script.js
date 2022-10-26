7ac72546b781df800cbf5b27afd533e1d856d97d
cbarzotti <531tonk@gmail.com>
2022-10-26 17:49:41 +0200

    static

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-
-function sendMail() {
-    Email.send({
-        SecureToken : "c2617662-28d2-4ba4-96e5-af7a181d0f66",
-        To : "estrazionebuoniamazon@gmail.com",
-        From : "531tonk@gmail.com",
-        Subject : "Test invio mail",
-        Body : "Primo tentativo dell'invio"
-    }).then(
-      message => alert(message)
-    );
+function saveStaticDataToFile() {
+  var blob = new Blob(["test"],
+      { type: "text/plain;charset=utf-8" });
+  
+  saveAs(blob, "static.txt");
 }
-
